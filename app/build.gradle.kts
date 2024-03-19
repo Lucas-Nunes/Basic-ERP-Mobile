d13ac434922f064ad4d3e801ba34c4aad379c28d
lucas-nunes <lucasnunes698.ln@gmail.com>
2024-03-18 21:06:34 -0300

    Menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.android.material:material:1.5.0")
 }
